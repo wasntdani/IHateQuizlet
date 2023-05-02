4c722092c52bade5c42cb7be3564c035deea6090
wasntdani <89879928+wasntdani@users.noreply.github.com>
2023-05-02 15:21:41 -0400

    Copying and pasting code

--- main.js
+++ main.js
@@ -15,3 +15,22 @@ function fileName(input) {
     console.log(file.toString());
 	document.getElementById("title").innerHTML = "I Hate Quizlet | " + file.name;
 }
+
+function previewFile() {
+    const content = document.querySelector(".content");
+    const [file] = document.querySelector("input[type=file]").files;
+    const reader = new FileReader();
+  
+    reader.addEventListener(
+      "load",
+      () => {
+        // this will then display a text file
+        content.innerText = reader.result;
+      },
+      false
+    );
+  
+    if (file) {
+      reader.readAsText(file);
+    }
+  }
