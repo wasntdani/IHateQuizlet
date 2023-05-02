3330c2380b5e103fe72f99d2103aa3a87191d37f
wasntdani <89879928+wasntdani@users.noreply.github.com>
2023-05-02 12:37:12 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let deck = document.getElementById("deck");
+
+// other decks
+fetch('./data.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+// Figure Out How To Save A File Into Decks Folder
+
+document.getElementById("title").innerHTML = "I Hate Quizlet | Please Upload A Deck";
+
+// Name the button after a file in decks
+function fileName(input) {
+	let file = input.files[0]; 
+	document.getElementById("title").innerHTML = "I Hate Quizlet | " + file.name;
+}
