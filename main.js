c34fb7a77951ab0edf91b0e11cf2de26112b10bd
leham39 <71613199+leham39@users.noreply.github.com>
2023-05-02 20:46:16 -0400

    fixes

--- main.js
+++ main.js
@@ -3,14 +3,15 @@ let deck = document.getElementById("deck");
 // other decks
 fetch('./data.json')
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((json) => console.log(json))
+    .then((json) => {const deckTest = json});
 
 // Figure Out How To Save A File Into Decks Folder
-
+console.log(deckTest);
 document.getElementById("title").innerHTML = "I Hate Quizlet | Please Upload A Deck";
 
 // This is where i'm going to test reading from data.json
-const firstDeck = JSON.parse(data.json);
+const firstDeck = JSON.parse(deckTest);
 console.log("JSON parse result:");
 console.log(firstDeck);
 document.getElementById("demo").innerHTML = firstDeck.term[0];
