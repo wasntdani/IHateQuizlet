a65349bf4733881ee70b07665dffc9407ac6c74e
leham39 <71613199+leham39@users.noreply.github.com>
2023-05-02 22:00:38 -0400

    parsing change

--- main.js
+++ main.js
@@ -17,10 +17,10 @@ getDecks()
   // This is where i'm going to test reading from data.json
   const firstDeck = JSON.parse(deckTest);
   console.log("JSON parse result:");
-  console.log(firstDeck);
-  document.getElementById("demo").innerHTML = firstDeck.term[0];
-  console.log(firstDeck.term[0]);
-  console.log(firstDeck.name);
+  console.log(deckTest);
+  document.getElementById("demo").innerHTML = deckTest.term[0];
+  console.log(deckTest.term[0]);
+  console.log(deckTest.name);
 });
 
 // This just displays the text in a txt file; it's not that special.
